3a3eeb7f06207f5ed78366d78b2c52732619f87f
TKman <102001532+greekr4@users.noreply.github.com>
2025-07-30 18:31:55 +0900

    feat: API

--- src/options/options.module.ts
+++ src/options/options.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { OptionsService } from './options.service';
 import { OptionsController } from './options.controller';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [OptionsController],
   providers: [OptionsService],
 })
